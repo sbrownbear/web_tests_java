7307afc59dddd86c133333ee0d11af7e884928b8
sbrownbear <sergey.gsarh@gmail.com>
2024-02-10 18:47:38 +0300

    added selenide code

--- README.md
+++
@@ -1,1 +0,0 @@
-# web_tests_java
---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,79 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        String firstName = "Sergey",
+                lastName = "Konoplev",
+                userEmail = "sergeyKonoplev@gmail.com",
+                gender = "Male",
+                userNumber = "9875036934",
+                fullDateOfBirth = "08 May,1996",
+                dayOfBirth = "08",
+                monthOfBirth = "May",
+                yearOfBirth = "1996",
+                subjects = "Maths",
+                hobbies = "Sports",
+                picture = "img/1.png",
+                verifyPicture = "1.png",
+                currentAddress = "Russia",
+                state = "NCR",
+                city = "Delhi",
+                stateAndCity = state + " " + city;
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        Selenide.executeJavaScript("$('#RightSide_Advertisement').remove()");
+        Selenide.executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth + ":not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+        $("#submit").click();
+
+        $(".modal-content").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text(firstName), text(lastName), text(userEmail), text(gender),
+                text(userNumber), text(fullDateOfBirth), text(subjects), text(hobbies),
+                text(verifyPicture), text(currentAddress), text(stateAndCity));
+    }
+}
+
+
