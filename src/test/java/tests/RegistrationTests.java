dbc58ef0779d88a92b5f343268182e4ea21e29af
sbrownbear <sergey.gsarh@gmail.com>
2024-02-11 22:46:53 +0300

    added allure, addListener, labels, and sreentshot

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,5 +1,8 @@
 package tests;
 
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationTests extends TestBase {
@@ -7,6 +10,11 @@ public class RegistrationTests extends TestBase {
     TestData data = new TestData();
 
     @Test
+    @Feature("Создание аккаунта пользователя в системе")
+    @Owner("Коноплев Сергей")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "DemoQA", url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Авторизация пользователя")
     void successfulRegistrationTest() {
 
         registrationPage.openPage()
@@ -15,6 +23,78 @@ public class RegistrationTests extends TestBase {
                 .setEmail(data.userEmailData)
                 .setGender(data.genderData)
                 .setNumber(data.userNumberData)
+                .setBirthDay(data.dayOfBirthData, data.monthOfBirthData, data.yearOfBirthData)
+                .setSubjects(data.subjectsData)
+                .setHobbies(data.hobbiesData)
+                .setUploadPic(data.pictureData)
+                .setCurrentAddress(data.currentAddressData)
+                .setState(data.stateData)
+                .setCity(data.cityData)
+                .setSubmit();
+
+        registrationPage.verifyResultModalAppears()
+                .verifyResults("Student Name", data.fullNameDate)
+                .verifyResults("Student Email", data.userEmailData)
+                .verifyResults("Gender", data.genderData)
+                .verifyResults("Mobile", data.userNumberData)
+                .verifyResults("Date of Birth", data.fullDateOfBirthData)
+                .verifyResults("Subjects", data.subjectsData)
+                .verifyResults("Hobbies", data.hobbiesData)
+                .verifyResults("Picture", data.verifyPictureData)
+                .verifyResults("Address", data.currentAddressData)
+                .verifyResults("State and City", data.stateAndCityData);
+    }
+
+    @Test
+    @Feature("Создание аккаунта пользователя в системе")
+    @Owner("Коноплев Сергей")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "DemoQA", url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Авторизация пользователя")
+    void unsuccessfulTest() {
+
+        registrationPage.openPage()
+                .setFirstName(data.firstNameData)
+                .setLastName(data.lastNameData)
+                .setEmail(data.userEmailData)
+                .setGender(data.genderData)
+                .setNumber(data.userEmailData) // error
+                .setBirthDay(data.dayOfBirthData, data.monthOfBirthData, data.yearOfBirthData)
+                .setSubjects(data.subjectsData)
+                .setHobbies(data.hobbiesData)
+                .setUploadPic(data.pictureData)
+                .setCurrentAddress(data.currentAddressData)
+                .setState(data.stateData)
+                .setCity(data.cityData)
+                .setSubmit();
+
+        registrationPage.verifyResultModalAppears()
+                .verifyResults("Student Name", data.fullNameDate)
+                .verifyResults("Student Email", data.userEmailData)
+                .verifyResults("Gender", data.genderData)
+                .verifyResults("Mobile", data.userNumberData)
+                .verifyResults("Date of Birth", data.fullDateOfBirthData)
+                .verifyResults("Subjects", data.subjectsData)
+                .verifyResults("Hobbies", data.hobbiesData)
+                .verifyResults("Picture", data.verifyPictureData)
+                .verifyResults("Address", data.currentAddressData)
+                .verifyResults("State and City", data.stateAndCityData);
+    }
+
+    @Test
+    @Feature("Создание аккаунта пользователя в системе")
+    @Owner("Коноплев Сергей")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "DemoQA", url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Авторизация пользователя")
+    @Disabled("Пропускаем тест по какой-либо причине")
+    void skipTest() {
+        registrationPage.openPage()
+                .setFirstName(data.firstNameData)
+                .setLastName(data.lastNameData)
+                .setEmail(data.userEmailData)
+                .setGender(data.genderData)
+                .setNumber(data.userEmailData) // error
                 .setBirthDay(data.dayOfBirthData, data.monthOfBirthData, data.yearOfBirthData)
                 .setSubjects(data.subjectsData)
                 .setHobbies(data.hobbiesData)
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -14,5 +22,16 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void getScreenshot() {
+        Allure.getLifecycle().addAttachment("Screenshot", "image/png","png",
+                ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES));
     }
 }
